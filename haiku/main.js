fe7ab541e5450dc2e45a4c4827df602c9c51f844
cindyn1264 <cindynguyen@cca.edu>
2024-03-31 19:50:42 -0700

    haiku

---
+++ haiku/main.js
@@ -0,0 +1,31 @@
+// Parallax Scroll 
+
+let stars = document.getElementById('stars');
+let wave = document.getElementById('wave');
+
+
+window.addEventListener('scroll', function(){
+      let value = window.scrollY;
+      stars.style.left = value * 0.1 + 'px';
+      wave.style.top = value * 0.7 + 'px';
+      })
+
+
+// Color changing text
+
+const text = document.querySelector('h2');
+text.style.color = '#ffffff';
+
+window.addEventListener('scroll', function() {
+      const scrollPosition = window.pageYOffset;
+      const maxScrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      const colorTransition = scrollPosition / maxScrollHeight;
+
+      const red = Math.floor(255 * (1 - colorTransition) + 32 * colorTransition);
+      const green = Math.floor(255 * (1 - colorTransition) + 25 * colorTransition);
+      const blue = Math.floor(255 * (1 - colorTransition) + 97 * colorTransition);
+
+      const hexColor = '#' + red.toString(16).padStart(2, '0') + green.toString(16).padStart(2, '0') + blue.toString(16).padStart(2, '0');
+
+      text.style.color = hexColor;
+});
